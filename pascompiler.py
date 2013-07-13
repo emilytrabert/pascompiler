792b71e5fec53ec221b97af6d5a532b1f8f01114
Emily Trabert <Emily@emily-trabert.local>
2013-07-13 14:14:40 -0400

    Added x86 code code to compiler and basic.pas to test cases.

--- pascompiler.py
+++ pascompiler.py
@@ -48,7 +48,7 @@ def supersplit(words):
 
 def lex(pasfile):
     sourcecode = openfile(pasfile)
-    print(sourcecode)
+    #print(sourcecode)
     postlex = ""
     symcount = 0
     combo = False
@@ -85,6 +85,9 @@ def lex(pasfile):
 
     return postlex
 
+def moveToNext(p):
+    return p + 1
+
 def getReservedWord(pointer, postlexarray):
     for key in reservedwords:
         if reservedwords[key] == postlexarray[pointer]:
@@ -112,12 +115,13 @@ def getIdentifier(pointer, postlexarray):
             return key
     return -1
 
-def syntaxError():
-    print "If you're happy and you know it, syntax error."
+def syntaxError(expected, got):
+    print "Expected:", expected
+    print "Encountered:", got
 
 def syntax(postlex):
     postlexarray = postlex.split()
-    print postlexarray
+    #print postlexarray
     pointer = 0
     while pointer < len(postlexarray):
         if getReservedWord(pointer, postlexarray) == "":
@@ -136,11 +140,60 @@ def syntax(postlex):
         pointer += 1
     return 0
 
+def bettersyntax(postlex):
+    postlexarray = postlex.split()
+    x86 = ""
+    pointer = 0
+    # if first token not program,error
+    if getReservedWord(pointer, postlexarray) != "PROGRAM":
+        syntaxError("PROGRAM", getReservedWord(pointer, postlexarray))
+        return
+
+    pointer = moveToNext(pointer)
+    
+    # second idenifier, third ;, fourth begin, fifth end.
+    if getIdentifier(pointer, postlexarray) == -1:
+        syntaxError("indentifier", getIndentifier(pointer, postlexarray))
+        return
+
+    pointer = moveToNext(pointer)
+
+    if getSymbol(pointer, postlexarray) != ';':
+        syntaxError(";", getSymbol(pointer, postlexarray))
+        return
+
+    pointer = moveToNext(pointer)
+
+    if getReservedWord(pointer, postlexarray) != "BEGIN":
+        syntaxError("BEGIN", getReservedWord(pointer, postlexarray))
+        return
+
+    x86 += """assume  cs:code, ds:code
+code	segment
+start:
+"""
+
+    pointer = moveToNext(pointer)
+
+    if getReservedWord(pointer, postlexarray) != "END.":
+        syntaxError("END.", getReservedWord(pointer, postlexarray))
+        return
+
+    x86 += """code	ends
+end	start"""
+
+    return x86
+
+    # fourth begin or var
+
+    # if var, call var function
+
 def main():
     pasfile = raw_input("File? ")
     postlex = lex(pasfile)
     print postlex
     print symboltable
-    syntax(postlex)
+    #syntax(postlex)
+    print(bettersyntax(postlex))
 
 main()
